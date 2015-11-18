39947d5b94d2122adbe2eb55582e65804f824f39
Michal Seta <mis@artengine.ca>
2015-11-18 16:22:18 -0500

    first commit

---
+++ __init__.py
@@ -0,0 +1,70 @@
+import bpy
+
+bl_info = {
+    "name": "SATIE OSC",
+    "author": "Michal Seta",
+    "version": (0, 1, 1),
+    "blender": (2, 75, 0),
+    "warning": "Preliminary version. You should have SATIE up and running to use this functionnality. It might work.",
+    "wiki_url": "http://code.sat.qc.ca/redmine/projects/sc-basic-renderer/wiki",
+    "description": "Connect a blender object with SATIE sound engine and author sound spacialisation.",
+    "category": "User",
+}
+
+from . import properties as SatieProperties
+from . import satieSynth as ss
+
+addon_keymaps = []
+
+def register():
+    print("registering SatieSynth")
+    bpy.utils.register_class(SatieProperties.SatieProperties)
+    bpy.utils.register_class(ss.SatieObject)
+
+    bpy.types.Object.useSatie = bpy.props.BoolProperty(
+                name = "Use SATIE",
+                description = "Make object interact with SATIE",
+                default = False
+            )
+    bpy.types.Object.satieID = bpy.props.StringProperty(
+                name = "ID",
+                description = "Sound source ID",
+                default = "mySource"
+            )
+    bpy.types.Object.satieURI = bpy.props.StringProperty(
+                name = "URI",
+                description = "SATIE plugin to use",
+                default = "plugin://default"
+            )
+    bpy.types.Object.sendToSATIE = bpy.props.BoolProperty(
+                name = "Send to SATIE",
+                description = "Send data to SATIE",
+                default = False
+            )
+    bpy.types.Object.state = bpy.props.BoolProperty(
+                name = "Source playing state",
+                description = "Playing state (on, off)",
+                default = False
+    )
+
+    # handle the keymap
+    wm = bpy.context.window_manager
+    km = wm.keyconfigs.addon.keymaps.new(name='Object Mode', space_type='EMPTY')
+    kmi = km.keymap_items.new(ss.SatieObject.bl_idname, 'SPACE', 'PRESS', ctrl=True, shift=True)
+    addon_keymaps.append((km, kmi))
+#end register
+
+def unregister():
+    print("unregistering SatieSynth")
+    for km, kmi in addon_keymaps:
+        km.keymap_items.remove(kmi)
+    addon_keymaps.clear()
+    bpy.utils.unregister_class(SatieProperties.SatieProperties)
+    bpy.utils.unregister_class(ss.SatieObject)
+    ss.cleanCallbackQueue()
+    bpy.types.Object.useSatie
+    bpy.types.Object.satieID
+    bpy.types.Object.satieURI
+
+if __name__ == "__main__":
+    register()
---
+++ properties.py
@@ -0,0 +1,27 @@
+import bpy
+
+class SatieProperties(bpy.types.Panel):
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+    bl_label = "SATIE properties"
+#    def draw(self, context) :
+#        TheCol = self.layout.column(align = True)
+#        TheCol.prop(context.scene, "make_satie_properties")
+#        TheCol.operator("mesh.add_satie_properties", text = "SATIE properties")
+#        TheCol.prop(self, "useSatie")
+#        TheCol.prop(self, "satieID")
+ 
+    def draw(self, context) :
+        objs = context.selected_objects
+        if len(objs) is not 1:
+            self.layout.label('Select exactly ONE object')
+        else:
+            TheCol = self.layout.column(align = True)
+            #TheCol.prop(context.scene, "make_satie_properties")
+            #TheCol.operator("mesh.add_satie_properties", text = "SATIE properties")
+            TheCol.prop(context.object, "useSatie")
+            TheCol.prop(context.object, "satieID")
+            TheCol.prop(context.object, "satieURI")
+            TheCol.prop(context.object, "sendToSATIE")
+            TheCol.prop(context.object, "state")
